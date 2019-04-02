482ae0f8a43ab0ce8b25e9ecd0d3712be1fa6cf1
Samrith Shankar <samrith.298@gmail.com>
2019-04-02 19:01:30 +0530

    Styles structurization

---
+++ src/structurers/styles.structurize.js
@@ -0,0 +1,48 @@
+const Path = require('path');
+const fs = require('fs');
+const move = require('glob-move');
+const chalk = require('chalk');
+
+const { extractFileName } = require('../util');
+
+module.exports = function({ dist, prefix, options, markups }) {
+    return new Promise(resolve => {
+        const { folder, match } = options;
+
+        move(`${dist}/${match}`, `${dist}/${folder}`)
+            .then(async () => {
+                await markups.forEach(async document => {
+                    const allStyles = document.querySelectorAll('link');
+
+                    const path = `${prefix}/${folder}/`;
+
+                    await allStyles.forEach(async style => {
+                        const oldFilePath = style.href;
+                        const fileName = extractFileName(oldFilePath);
+                        const stylePath = Path.join(dist, folder, fileName);
+                        const content = await fs
+                            .readFileSync(stylePath)
+                            .toString();
+
+                        style.href = `${path}${fileName}`;
+                        fs.writeFileSync(
+                            stylePath,
+                            content.replace(oldFilePath, style.href)
+                        );
+                    });
+                });
+
+                resolve();
+            })
+            .catch(e => {
+                console.log(
+                    chalk.red.bold(
+                        'Error while structurizing styles. Please check structures.'
+                    )
+                );
+                console.log(chalk.gray('Dist path:', dist));
+                console.log(e);
+                process.exit();
+            });
+    });
+};
