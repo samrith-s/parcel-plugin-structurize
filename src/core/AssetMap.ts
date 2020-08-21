ea4564537979c47c84fea5306fe777e2a204b230
Samrith Shankar <samrith.298@gmail.com>
2020-08-21 02:56:58 +0200

    Implemented AssetMap

--- src/core/AssetMap.ts
+++ src/core/AssetMap.ts
@@ -1,51 +1,65 @@
 import * as path from 'path';
+import * as minimatch from 'minimatch';
 
-import { ConfigProvider } from './providers/Config';
+import { ConfigProvider, ConfigInternal } from './providers/Config';
+import { ParcelBundle } from 'parcel-bundler';
 
-export interface AssetInfo {
-    file: string;
-    location: string;
-    path: string;
-}
-export interface Asset extends AssetInfo {
-    mapFile?: AssetInfo;
+export interface AssetGraph {
+    [key: string]: {
+        newPath: string | null;
+        mapFile: boolean;
+        dependents: string[] | null;
+    };
 }
 
 export class AssetMap extends ConfigProvider {
-    private assetsMap: Asset[] = [];
+    private assetsMap: AssetGraph = {};
+    private bundle: ParcelBundle;
 
-    constructor() {
+    constructor(bundle: ParcelBundle) {
         super();
+        this.bundle = bundle;
         this.generateAssetsMap();
     }
 
-    public get(): Asset[] {
+    public get(): AssetGraph {
         return this.assetsMap;
     }
 
     private generateAssetsMap() {
-        const bundleAssets = Array.from(this.bundler.bundleNameMap.values());
-        bundleAssets.forEach(name => {
-            if (path.extname(name) !== '.map') {
-                const { publicUrl = '/', outDir } = this.bundlerConfig;
-                const file = name;
-                const asset: Asset = {
-                    file,
-                    location: path.join(outDir, file),
-                    path: path.join(publicUrl, file)
-                };
+        if (this.bundle.entryAsset) {
+            this.iterativeDependencyResolver(this.bundle);
+        } else {
+            this.bundle.childBundles.forEach(asset => {
+                this.iterativeDependencyResolver(asset);
+            });
+        }
+    }
 
-                if (bundleAssets.includes(`${file}.map`)) {
-                    const mapFile = `${file}.map`;
-                    asset.mapFile = {
-                        file: mapFile,
-                        location: path.join(outDir, mapFile),
-                        path: path.join(publicUrl, mapFile)
-                    };
-                }
-
-                this.assetsMap.push(asset);
-            }
+    private iterativeDependencyResolver(depAsset: ParcelBundle): void {
+        const file = path.basename(depAsset.name);
+        const extension = path.extname(file);
+        const mapFile = extension === '.map';
+        const fileConfig = this.config.find(c => {
+            return minimatch.default(mapFile ? file.replace(/(\.map)$/, '') : file, c.match);
         });
+
+        this.assetsMap[file] = {
+            newPath: this.generatePath(file, fileConfig),
+            mapFile,
+            dependents: null
+        };
+
+        if (depAsset.childBundles.size) {
+            this.assetsMap[file].dependents = [];
+            depAsset.childBundles.forEach(childBundle => {
+                this.assetsMap[file].dependents.push(path.basename(childBundle.name));
+                this.iterativeDependencyResolver(childBundle);
+            });
+        }
+    }
+
+    private generatePath(file: string, fileConfig: ConfigInternal): string {
+        return fileConfig ? path.join(this.bundlerConfig.publicUrl, fileConfig.folder, file) : null;
     }
 }
