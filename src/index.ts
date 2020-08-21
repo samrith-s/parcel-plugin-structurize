3644b76c45df326eafccdf0be6572aee8d2b1170
Samrith Shankar <samrith.298@gmail.com>
2020-08-21 05:17:06 +0200

    Added console information and exception handling

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
 import ParcelBundler from 'parcel-bundler';
+import chalk from 'chalk';
 
-import { AssetMap } from './core/AssetMap';
 import { BundlerProvider } from './core/providers/Bundler';
+import { ConfigProvider } from './core/providers/Config';
+import { AssetMap } from './core/AssetMap';
+import { FileManager } from './core/FileManager';
 
 export default function Structurize(bundler: ParcelBundler): void {
     if (process.env.NODE_ENV === 'production') {
+        process.on('uncaughtException', errorHandler);
+        process.on('unhandledRejection', errorHandler);
+
         BundlerProvider.init(bundler);
 
         bundler.on('bundled', bundle => {
-            console.log(__dirname);
-            console.log(process.cwd());
+            ConfigProvider.init();
+            console.log(chalk`
+ {cyan parcel-plugin-structurize}
+ {dim Config: ${ConfigProvider.config.filepath}}
+        `);
             const map = new AssetMap(bundle);
-            console.log(map.get());
+            const fm = new FileManager(map.get());
+            fm.structurize();
         });
     }
+}
+
+function errorHandler(error: Error) {
+    console.log(chalk`
+ {yellow The plugin "parcel-plugin-custom-dist-structure" has encountered an error.}
+
+ {red ${error.stack}}
+
+ {dim If you think this is a bug, use this link to report it on Github.
+ http://bit.ly/parcel-plugin-structurize-bug}
+    `);
 }
