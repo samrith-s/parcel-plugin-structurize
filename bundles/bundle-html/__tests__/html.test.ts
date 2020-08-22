ca12a4d3f2b745debce7f6d6c5433805be885ce9
Samrith Shankar <samrith.298@gmail.com>
2020-08-22 03:14:33 +0200

    refactor: moved all bundles as a singular bundle into `__tests__`

--- bundles/bundle-html/__tests__/html.test.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @bundle-name bundle-html
- * @bundle-entry src/index.html, src/app.html
- * @bundle-run-setup true
- */
-
-test('Build structure is ok', () => {
-    expect(1 + 1).toEqual(2);
-});
