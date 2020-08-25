9f2efabfa38c0496495d8d78c0306ac98b1c8c46
Samrith Shankar <samrith.298@gmail.com>
2020-08-26 01:20:47 +0200

    chore: update lockfiles and prepare-test

--- scripts/prepare-test.sh
+++ scripts/prepare-test.sh
@@ -1,5 +1,4 @@
-rm -rf yarn.lock
 yarn install;
 yarn link;
-(cd __tests__/bundle && rm -rf yarn.lock && yarn link parcel-plugin-structurize && yarn install);
+(cd __tests__/bundle && yarn link parcel-plugin-structurize && yarn install);
 rm -rf ./.yarn-metadata.json
