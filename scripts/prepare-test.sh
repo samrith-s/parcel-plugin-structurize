fb88974efd670873c4604be71ba1e9bb2c7833a1
Samrith Shankar <samrith.298@gmail.com>
2020-08-26 01:58:52 +0200

    tests: switched test commands from npm to yarn

--- scripts/prepare-test.sh
+++ scripts/prepare-test.sh
@@ -1,4 +1,4 @@
-npm install;
-npm run build;
-npm link;
-(cd __tests__/bundle && npm link parcel-plugin-structurize && npm install);
+yarn install;
+yarn build;
+yarn link;
+(cd __tests__/bundle && yarn link parcel-plugin-structurize && yarn install);
