471381c3f35b763e2268e533e9e2502ddbb47478
Samrith Shankar <samrith.298@gmail.com>
2020-08-26 01:16:33 +0200

    Fixed prepare-test

--- scripts/prepare-test.sh
+++ scripts/prepare-test.sh
@@ -1,5 +1,5 @@
-yarn install;
 rm -rf yarn.lock
+yarn install;
 yarn link;
 (cd __tests__/bundle && rm -rf yarn.lock && yarn link parcel-plugin-structurize && yarn install);
 rm -rf ./.yarn-metadata.json
